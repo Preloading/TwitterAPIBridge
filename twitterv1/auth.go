a56928b525acb67150515a08e9a22e705dcabc51
Logan <60761520+Preloading@users.noreply.github.com>
2025-01-04 00:38:25 -0700

    fix: return "proper" user handle thru oauth_token

--- twitterv1/auth.go
+++ twitterv1/auth.go
@@ -3,6 +3,7 @@ package twitterv1
 import (
 	"errors"
 	"fmt"
+	"net/url"
 	"regexp"
 	"strings"
 	"time"
@@ -71,7 +72,7 @@ func access_token(c *fiber.Ctx) error {
 			Timestamp:            time.Now(),
 		})
 
-		return c.SendString(fmt.Sprintf("oauth_token=%s&oauth_token_secret=%s&user_id=%s&screen_name=twitterapi&x_auth_expires=%f", oauth_token, oauth_token, fmt.Sprintf("%d", bridge.BlueSkyToTwitterID(res.DID)), *access_token_expiry))
+		return c.SendString(fmt.Sprintf("oauth_token=%s&oauth_token_secret=%s&user_id=%s&screen_name=%s&x_auth_expires=%f", oauth_token, oauth_token, fmt.Sprintf("%d", bridge.BlueSkyToTwitterID(res.DID)), url.QueryEscape(authUsername), *access_token_expiry))
 	}
 	// We have an unknown request. huh. Probably registration, i'll find a way to send an error msg for that later, as registration is out of scope.
 	return c.SendStatus(501)
